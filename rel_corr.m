99e2e1cfa6f096b9c02f3ed419506c98616a1d99
minoru-kagamida <37274439+minoru-kagamida@users.noreply.github.com>
2018-03-11 20:29:53 -0400

    relativistic error/correction
    Could only find this formula, might also need to cross reference the source.

---
+++ rel_corr.m
@@ -0,0 +1,10 @@
+function [delta_rel] = rel_corr(SortedData, Mk)
+%Source: http://www.navipedia.net/index.php/Relativistic_Clock_Correction#cite_note-3
+A = semi_major(SortedData); %semi-major axis
+mu = 3.986005e14; %grav const
+c = physconst('LightSpeed'); %speed of light
+Ek = ecc_anom(SortedData, Mk); %eccentric anomaly
+e = SortedData(:,33); %eccentricity
+
+delta_rel = -2.*(sqrt(mu.*A)./(c^2)).*e.*sin(Ek);
+end
