0241fc384b0c380f016c8223109d41a31acd5e1e
minoru-kagamida <37274439+minoru-kagamida@users.noreply.github.com>
2018-03-11 22:11:14 -0400

    Clock correction (rough)
    It runs, but im pretty sure i might be calling in the wrong data for the time variables

---
+++ clock_corr.m
@@ -0,0 +1,26 @@
+function [delta_Tsv_C1, delta_Tsv_P1, delta_Tsv_P2] = clock_corr(SortedData, RcvrtC1,RcvrtP1,RcvrtP2, delta_rel)
+%Sources
+%http://www.navipedia.net/index.php/Clock_Modelling
+%
+
+a0 = SortedData(:,25); %SV clock bias or offset???
+a1 = SortedData(:,26); %SV clock drift
+a2 = SortedData(:,27); %SV clock drift rate
+
+%Not sure about these time variables...
+%Either clock data reference time (epoch time of obs)
+%...or could be receiver time.
+%...or could be the reference time in the header for the file?
+%...in this case the receiver time is used for the sake of completion.
+Toc_C1 = RcvrtC1;
+Toc_P1 = RcvrtP1;
+Toc_P2 = RcvrtP2;
+
+%Not sure if using Nav GPS time or Obs GPS time.
+%...in this case, using Nav GPS time for completion.
+t = SortedData(:,26);
+
+delta_Tsv_C1 = a0 + a1.*(t-Toc_C1)+a2.*(t-Toc_C1).^2+delta_rel;
+delta_Tsv_P1 = a0 + a1.*(t-Toc_P1)+a2.*(t-Toc_P1).^2+delta_rel;
+delta_Tsv_P2 = a0 + a1.*(t-Toc_P2)+a2.*(t-Toc_P2).^2+delta_rel;
+end
